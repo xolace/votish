215619f1b97316aadf8fc2de227cd3391400ff87
xol <xikkol@gmail.com>
2011-10-08 12:50:59 -1000

    Removed tally, and implemented  vote and singer associations for assignment and counting.

--- votish.rb
+++ votish.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'sinatra'
 require 'datamapper'
 
@@ -8,54 +9,66 @@ class Vote
   include DataMapper::Resource
   property :id, Serial
   property :cell, String
-  property :ballot, String
   property :date, DateTime
-  
+  belongs_to :singer
 end
 
-Vote.auto_migrate! unless Vote.storage_exists?
 
 class Singer
   include DataMapper::Resource
   property :id, Serial
   property :name, String
-  property :tally, Integer , :default => 0
+  has n, :votes
+def counting
+  puts self.relationships[:votes].inspect
+end
+
 end
 
 Singer.auto_migrate! unless Singer.storage_exists?
+Vote.auto_migrate! unless Vote.storage_exists?
+
+
+class Admin
+  include DataMapper::Resource
+  property :id, Serial
+  property :username, String
+  property :password, String
+  property :elevation, Integer
+end
+
 
 helpers do
 
 end
 
+get '/test' do
+  singer = Singer.first
+  "#{singer.name} has #{singer.votes.count}"
+  puts Singer.relationships[:votes].counting
+end
 
 get '/vote/:cell/:ballot' do
-     check = Vote.first( :cell => "#{params[:cell]}" )
-     if check.nil?
-     "Vote cast"
-     @singer = Singer.first( :id => "#{params[:ballot].to_i}" )
-     @post = Vote.create(
-      :cell => "#{params[:cell]}",
-      :ballot  => "#{params[:ballot]}",
-      :date => Time.now
-       )
-    @singer.tally += 1
-    @singer.save
+  check = Vote.first(:cell => "#{params[:cell]}")
+  if check.nil?
+    @post = Vote.create(
+        :cell => "#{params[:cell]}",
+        :singer => Singer.first(:id => "#{params[:ballot].to_i}"),
+        :date => Time.now
+    )
     @post.save
+    @message = "Your vote has been cast for #{@post.singer.name}. Thanks! (Ballot cast from #{@post.cell}"
+  else
+    unless check.singer.id == params[:ballot]
+      rejected = check.singer.name
+      check.update(:singer => Singer.first(:id => "#{params[:ballot].to_i}"))
+      check.save
+      @message = "Your vote has been changed from #{rejected} to #{check.singer.name}. (Ballot change from #{check.cell})"
     else
-    rejected = Singer.first(:id => "#{check.ballot.to_i}")
-    singer = Singer.first(:id => "#{params[:ballot]}")
-    unless rejected == singer
-    rejected.tally -= 1;
-    singer.tally += 1;
-    check.ballot = params[:ballot].to_i
-    rejected.save
-    singer.save
-    check.save
-end
-    "Vote changed for this number has changed from #{rejected.name} (Votes: #{rejected.tally}) to #{singer.name} (Votes: #{singer.tally}). These tallies reflect your change of vote."
-end
-"Thanks for your vote"
+      @message = "Your vote for #{check.singer.name} remains unchanged. (The input we received was '#{params[:ballot]}"
+    end
+  end
+  "#{@message}"
 end
 
 
@@ -65,19 +78,19 @@ end
 
 post '/admin/person/delete' do
   if params[:password] == "vitaminwater"
-  @person = Singer.first( :id => "#{params[:id].to_i}" )
-  erb :rem
-  @person.destroy
-  "Person deleted"
+    @person = Singer.first(:id => "#{params[:id].to_i}")
+    erb :rem
+    @person.destroy
+    "Person deleted"
   else
-  "Permission denied"
-end
+    "Permission denied"
+  end
 end
 
 get '/' do
-   @singers = Singer.all(:order => [:tally.desc ] )
-   erb :results
-   end
+  @singers = Singer.all.sort_by { |singer| singer.votes.count }
+  erb :results
+end
 
 get '/admin' do
   erb :admin
@@ -90,16 +103,16 @@ post '/admin' do
       votes = Vote.all
       singers.each do |singer|
         singer.tally = 0
-	singer.save
+        singer.save
       end
       votes.destroy
-     "Tallies have been reset, and validation table cleared. The voting may commence again."
+      "Tallies have been reset, and validation table cleared. The voting may commence again."
     end
-   unless params[:hardreset].nil?
-     Vote.auto_migrate!
-     Singer.auto_migrate!
-     "Clean as a baby's bottom. Everything has been erased. Everything."
-   end
+    unless params[:hardreset].nil?
+      Vote.auto_migrate!
+      Singer.auto_migrate!
+      "Clean as a baby's bottom. Everything has been erased. Everything."
+    end
   else
     "Permission denied"
   end
@@ -109,12 +122,12 @@ get '/admin/person/add' do
   erb :add
 end
 
-post '/admin/person/add' do 
+post '/admin/person/add' do
   if params[:password] == "vitaminwater"
-  @person = Singer.new( :name => "#{params[:name]}" )
-  @person.save
-  erb :create
+    @person = Singer.new(:name => "#{params[:name]}")
+    @person.save
+    erb :create
   else
-  "Permission denied"
+    "Permission denied"
   end
 end 
