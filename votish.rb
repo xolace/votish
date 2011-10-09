6a8d35eedbefc26143c49febb73fc9dd14efc1b3
xol <xikkol@gmail.com>
2011-10-08 14:12:46 -1000

    v1

--- votish.rb
+++ votish.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/session'
 require 'datamapper'
 
 #DataMapper::setup(:default, "")
@@ -19,9 +20,10 @@ class Singer
   property :id, Serial
   property :name, String
   has n, :votes
-def counting
-  puts self.relationships[:votes].inspect
-end
+
+  def counting
+    puts self.relationships[:votes].inspect
+  end
 
 end
 
@@ -42,6 +44,9 @@ helpers do
 
 end
 
+set :session_fail, '/login'
+set :session_secret, 'secretcodehere'
+
 get '/test' do
   singer = Singer.first
   "#{singer.name} has #{singer.votes.count}"
@@ -50,54 +55,85 @@ end
 
 get '/vote/:cell/:ballot' do
   check = Vote.first(:cell => "#{params[:cell]}")
-  if check.nil?
-    @post = Vote.create(
-        :cell => "#{params[:cell]}",
-        :singer => Singer.first(:id => "#{params[:ballot].to_i}"),
-        :date => Time.now
-    )
-    @post.save
-    @message = "Your vote has been cast for #{@post.singer.name}. Thanks! (Ballot cast from #{@post.cell}"
-  else
-    unless check.singer.id == params[:ballot]
-      rejected = check.singer.name
-      check.update(:singer => Singer.first(:id => "#{params[:ballot].to_i}"))
-      check.save
-      @message = "Your vote has been changed from #{rejected} to #{check.singer.name}. (Ballot change from #{check.cell})"
+  if Singer.get(params[:ballot])
+    if check.nil?
+      @post = Vote.create(
+          :cell => "#{params[:cell]}",
+          :singer => Singer.first(:id => "#{params[:ballot].to_i}"),
+          :date => Time.now
+      )
+      @post.save
+      @message = "Your vote has been cast for #{@post.singer.name}. Thanks! (Ballot cast from #{@post.cell})"
     else
-      @message = "Your vote for #{check.singer.name} remains unchanged. (The input we received was '#{params[:ballot]}"
+      unless check.singer.id == params[:ballot]
+        rejected = check.singer.name
+        check.update(:singer => Singer.first(:id => "#{params[:ballot].to_i}"))
+        check.save
+        @message = "Your vote has been changed from #{rejected} to #{check.singer.name}. (Ballot change from #{check.cell})"
+      else
+        @message = "Your vote for #{check.singer.name} remains unchanged. (The input we received was '#{params[:ballot]})"
+      end
     end
+  else
+    @message = "You have made an invalid selection."
   end
   "#{@message}"
 end
 
 
 get '/del/person/:id' do
-  erb :del
+  if session?
+    erb :del
+  else
+    redirect '/login'
+  end
 end
 
 post '/admin/person/delete' do
-  if params[:password] == "vitaminwater"
+  if session?
     @person = Singer.first(:id => "#{params[:id].to_i}")
-    erb :rem
-    @person.destroy
-    "Person deleted"
+    destroyed = @person.name
+    puts @person.votes.destroy
+    puts @person.destroy
+    @message = "#{destroyed} successfully removed."
+    @destiny = "/"
+    erb :redirect
   else
-    "Permission denied"
+    redirect "/login"
+  end
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  if params[:username] == "admin" && params[:password] = "vitaminwater"
+    session_start!
+    session[:name] = params[:username]
+    redirect '/'
+  else
+    @destiny = "/login"
+    @message = "Your username or password is incorrect."
+    erb :redirect
   end
 end
 
 get '/' do
-  @singers = Singer.all.sort_by { |singer| singer.votes.count }
+  @singers = Singer.all.sort_by { |singer| -singer.votes.count }
   erb :results
 end
 
 get '/admin' do
-  erb :admin
+  if session?
+    erb :admin
+  else
+    redirect '/login'
+  end
 end
 
 post '/admin' do
-  if params[:password] == "vitaminwater"
+  if session?
     @destiny = "/admin"
     unless params[:softreset].nil?
       votes = Vote.all
@@ -111,21 +147,29 @@ post '/admin' do
       @message = "Clean as a baby's bottom. Everything has been erased. Everything."
     end
   else
-    @message = "Permission denied"
+    redirect '/login'
   end
   erb :redirect
 end
 
 get '/admin/person/add' do
-  erb :add
+  if session?
+    erb :add
+  else
+    redirect '/login'
+  end
+
 end
 
 post '/admin/person/add' do
-  if params[:password] == "vitaminwater"
+  if session?
     @person = Singer.new(:name => "#{params[:name]}")
     @person.save
-    erb :create
+    @destiny = "/"
+    @message = "You have sucessfully added #{@person.name}"
+    erb :redirect
   else
-    "Permission denied"
+    @destiny = "/login"
+    @message = "Permission denied"
   end
 end 
